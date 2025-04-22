45510d201be2a6d7beae6e3abe2fb7c5213c85a7
Aditya312030 <adityaambadgatti2@gmail.com>
2025-04-22 12:50:41 +0530

    save

--- Simple.java
+++ Simple.java
@@ -1,6 +1,8 @@
 public class Simple {
     public static void main(String[] args) {
-     System.out.println("Main");   
+     System.out.println("Main");  
+     System.out.println("Master");  
+
     }
     
 }
